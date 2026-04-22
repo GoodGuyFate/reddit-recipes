75a1da5fcda46c3d59c3f8110bc20132ecc3d9e2
GoodGuyFate <xgamerfatex@gmail.com>
2026-04-23 00:22:44 +0200

    fix vercel routing

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -1,10 +1,7 @@
-// Path: api/lib/prisma.js
 import { PrismaClient } from '../../prisma/generated/prisma/client/index.js';
 
 const globalForPrisma = globalThis;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+globalForPrisma.prisma = prisma;
